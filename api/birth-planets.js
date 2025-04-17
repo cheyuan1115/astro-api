7249ba19ea035ee8d29b524ec20eadc6d0bea3f7
cheyuan1115 <cheyuan@gmail.com>
2025-04-17 16:32:51 +0800

    Add files via upload

--- api/birth-planets.js
+++ api/birth-planets.js
@@ -1,1 +1,17 @@
 
+import { getPlanetSignsAtNoon } from '../../utils/planetCalc.js';
+
+export default async function handler(req, res) {
+  const { date } = req.query;
+
+  if (!date) {
+    return res.status(400).json({ error: "Missing 'date' query parameter" });
+  }
+
+  try {
+    const signs = await getPlanetSignsAtNoon(date);
+    res.status(200).json(signs);
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error", detail: err.message });
+  }
+}
